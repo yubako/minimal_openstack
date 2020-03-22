8488953507210da8203e60cc45a36f0b83a677e8
user <user@email.com>
2020-03-22 03:10:58 -0400

    add rabbitmq test

--- tests/test_mariadb.py
+++ tests/test_mariadb.py
@@ -1,9 +1,19 @@
 
 def test_mariadb(host):
-    pkg_mariadb = host.package('mariadb')
-    assert pkg_mariadb.is_installed
+    assert host.package('mariadb').is_installed
 
     sv_mariadb = host.service('mariadb')
     assert sv_mariadb.is_running
     assert sv_mariadb.is_enabled
+
+def test_rabbitmq(host):
+    assert host.package('rabbitmq-server').is_installed
+
+    sv_rabbimq = host.service('rabbitmq')
+    assert sv_rabbitmq.is_running
+    assert sv_rabbitmq.is_enabled
+
+    assert "openstack" in host.check_output("rabbitmqctl list_users")
+
+
 
